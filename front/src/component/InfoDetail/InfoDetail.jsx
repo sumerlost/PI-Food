9a97d07639cb0da6daec2a30f8ff0931dc4ab3d5
sumerlost <Gaby_98@hotmail.com.ar>
2023-04-01 20:15:19 -0300

    sigo trabajando en otra pc

--- front/src/component/InfoDetail/InfoDetail.jsx
+++ front/src/component/InfoDetail/InfoDetail.jsx
@@ -6,8 +6,16 @@ export const InfoDetail = () => {
     return (
         <div className={styles.Workplace}>
             <div className={styles.Info}>
-                <div className={styles.Name}></div>
-                <div className={styles.Data}></div>
+                <div className={styles.InfoName}>
+                    <h2 className={styles.Name}>Sopita de lentejas</h2>
+                </div>
+                <div className={styles.Data1}>
+                    <h3 className={styles.InfoData1}>asd</h3>
+                    <h3 className={styles.InfoData1}>asd</h3>
+                </div>
+                <div className={styles.Data1}>
+                    <h3 className={styles.InfoData2}> Gluten free, Vegetarian, putoelquelee</h3>
+                </div>
             </div>
 
         </div>

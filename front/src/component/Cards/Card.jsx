e043fb799848f89122689473a1fe14bd556605fc
sumerlost <Gaby_98@hotmail.com.ar>
2023-04-05 04:28:26 -0300

    fixes

--- front/src/component/Cards/Card.jsx
+++ front/src/component/Cards/Card.jsx
@@ -1,13 +1,30 @@
 import React, { useEffect } from "react";
 import { Link, useParams } from "react-router-dom";
 import { RECIPEID } from "../../Routes/Routes";
-
+import styles from "./Card.module.css"
 
 export const Card = ({ recipe }) => {
+    // <Link to={RECIPEID(recipe.code)}></Link>
+    if (recipe) {
 
-    return (
-        <div>
-            {recipe ? <Link to={RECIPEID(recipe.code)}><img src={recipe.imagen} alt="" /></Link> : console.log("no funco")}
-        </div>
-    )
+        const diets = recipe.diets.map((element) => { return (<ul className={styles.Ul}>{element}</ul>) })
+
+        return (
+            <div className={styles.Esqueleto}>
+                <div className={styles.Top}>
+                    <img className={styles.Img} src={recipe.imagen} alt="" />
+                    <div className={styles.Diets}>
+                        <h4 className={styles.DietSub}>dietas</h4>
+                        <div className={styles.List}>{diets.map(e => e)}</div>
+                    </div>
+                </div>
+                <div className={styles.Bot}>
+                    <p>{recipe.title}</p>
+                </div>
+            </div>
+        )
+    }
+    else {
+        console.log("funca")
+    }
 }
--- front/src/component/Filters/ButtonFilter.jsx
+++ front/src/component/Filters/ButtonFilter.jsx
@@ -1,11 +1,11 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch } from "react-redux";
 import { getAllRecipes } from "../../Redux/actions/getallrecipes";
 
 export const Filter = () => {
     const dispatch = useDispatch()
 
-    const [toggleDiets, setToggleDiets] = useState({ "gluten%20free": false, "vegane": false, "primal": false })
+    const [toggleDiets, setToggleDiets] = useState({ "gluten%20free": false, "vegan": false, "primal": false })
 
     const handletoggle1 = () => {
         setToggleDiets({ ...toggleDiets, "gluten%20free": !toggleDiets["gluten%20free"] })
@@ -15,28 +15,36 @@ export const Filter = () => {
                 filtro.push(key)
             }
         }
-        dispatch(getAllRecipes(1, filtro))
+
     }
     const handletoggle2 = () => {
-        setToggleDiets({ ...toggleDiets, "vegane": !toggleDiets["vegane"] })
+        setToggleDiets({ ...toggleDiets, "vegan": !toggleDiets["vegan"] })
         const filtro = []
         for (let key in toggleDiets) {
             if (toggleDiets[key] === true) {
                 filtro.push(key)
             }
         }
-        dispatch(getAllRecipes(1, filtro))
+
     }
     const handletoggle3 = () => {
         setToggleDiets({ ...toggleDiets, "primal": !toggleDiets["primal"] })
+
+
+    }
+
+    useEffect(() => {
         const filtro = []
         for (let key in toggleDiets) {
             if (toggleDiets[key] === true) {
                 filtro.push(key)
             }
         }
+        if (filtro.length === 0) {
+            filtro.push("none")
+        }
         dispatch(getAllRecipes(1, filtro))
-    }
+    }, [toggleDiets])
 
     return (
         <div>
